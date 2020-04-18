6068e3421903c5eac774f790ab30fa9d172b65fa
Vanessa <vanne.jgs@gmail.com>
2020-04-18 12:30:46 -0500

    borrar y modificar listo

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -13,10 +13,10 @@ export class HttpService {
   httpConexion = "http://127.0.0.1:8000/";
 
   // FUNCION QUE RECIBE LOS PARAMETROS NECESARIOS
-  insertar(nombre: string,edad: string) {
+  insertar(nombre: string, edad: string) {
     // Complementamos la URL con la ruta
     // creada en laravel
-    var url = this.httpConexion + 'insertar/' + nombre + '/' + edad ;
+    var url = this.httpConexion + 'insertar/' + nombre + '/' + edad;
     return new Promise((resolve, reject) => {
       this.http.get(url)
         .subscribe(data => {
@@ -26,6 +26,64 @@ export class HttpService {
         });
     });
   }
+
+  // FUNCION QUE REALIZA LA CONSULTA
+  mostrarTodos() {
+    // Complementamos la URL con la ruta laravel
+    var url = this.httpConexion + 'traer1/';
+    //Método que retorna el resultado
+    return new Promise((resolve, reject) => {
+      this.http.get(url)
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  // FUNCION QUE ELIMINA UN REGISTRO
+  borrar(id:String) {
+    // Complementamos la URL con la ruta laravel
+    var url = this.httpConexion + 'eliminar/'+id;
+    //Método que retorna el resultado
+    return new Promise((resolve, reject) => {
+      this.http.get(url)
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  modificarNombre(id: string, nombre: string) {
+    // Complementamos la URL con la ruta laravel
+    var url = this.httpConexion + 'actualizarNombre/' + id + '/' + nombre;
+    return new Promise((resolve, reject) => {
+      this.http.get(url)
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  modificarEdad(id: string, edad: string) {
+    // Complementamos la URL con la ruta
+    // creada en laravel
+    var url = this.httpConexion + 'actualizarEdad/' + id + '/' + edad;
+    return new Promise((resolve, reject) => {
+      this.http.get(url)
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
 
 
 
